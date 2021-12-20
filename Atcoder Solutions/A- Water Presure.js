5d90976cfdc8d26705fe1edff8d79bceb0bb0a65
MohammedSboui <sbouimohamed8@gmail.com>
2021-12-20 17:06:10 +0100

    add waterPresure

---
+++ Atcoder Solutions/A- Water Presure.js
@@ -0,0 +1,29 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    
+    // here goes your code
+    const D = parseInt(readLine())
+    console.log(D/100);
+
+}
